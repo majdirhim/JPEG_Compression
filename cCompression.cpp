3801a9711b52e84e891b6ea84ea313d25d2be537
majdirhim <54720363+majdirhim@users.noreply.github.com>
2022-11-04 22:12:27 +0100

    Try IDCT

--- cCompression.cpp
+++ cCompression.cpp
@@ -57,7 +57,7 @@ double cCompression::DCT_Sum(int Block8x8[][Bloc8])const{
     return s;
 }
 /**
- * @brief Apply the Discrete cosine transform function to a block of image (8x8)
+ * @brief Apply the Discrete cosine transform function to a block of image 8x8
  * 
  * @param DCT_Img : pointer
  * @param Block8x8 : pointer
@@ -67,23 +67,33 @@ void cCompression::Calcul_DCT_Block(char (*Block8x8)[Bloc8],double(*DCT_Img)[Blo
     double s=0.0;
     for(unsigned int u=0;u<Bloc8;u++){
         for(unsigned int v=0;v<Bloc8;v++){
-                for(unsigned int x=0;x<Bloc8;x++)
-                    for(unsigned int y=0;y<Bloc8;y++)
-                        s+=*(*(Block8x8+x)+y)*cos((2*x+1)*M_PI)*cos((2*y+1)*M_PI)*u*v/16;
-            *(*(DCT_Img+u)+v)=(coeff(u)*coeff(v)/4)*s;
+            s=0.0;
+            for(unsigned int x=0;x<Bloc8;x++)
+                for(unsigned int y=0;y<Bloc8;y++)
+                    s+=*(*(Block8x8+x)+y)*cos((2*x+1)*M_PI*u/16.0)*cos((2*y+1)*M_PI*v/16.0);
+            *(*(DCT_Img+u)+v)=(coeff(u)*coeff(v)/4.0)*s;
         }
     }      
 
 }
 /**
- * @brief Apply the inverse discrete cosine transform to a DCT encoded image (8x8)
+ * @brief Apply the inverse discrete cosine transform to a DCT encoded image 8x8
  * 
  * @param DCT_Img 
  * @param Block8x8 
  */
 
 void cCompression::Calcul_iDCT(double(*DCT_Img)[Bloc8],char (*Block8x8)[Bloc8])const{
-
+    double s=0.0;
+    for(unsigned int x=0;x<Bloc8;x++){
+        for(unsigned int y=0;y<Bloc8;y++){
+            s=0.0;
+            for(unsigned int u=0;u<Bloc8;u++)
+                for(unsigned int v=0;v<Bloc8;v++)
+                    s+=*(*(DCT_Img+u)+v)*coeff(u)*coeff(v)*cos((2*x+1)*M_PI*u/16.0)*cos((2*y+1)*M_PI*v/16.0);
+            *(*(Block8x8+x)+y)=s/4.0;
+        }
+    }
 }
 
 
